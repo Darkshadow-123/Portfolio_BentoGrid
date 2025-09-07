bb037749cebe2037638fb8d4dd8c2d81fe3d4dce
Priyansh Raj Gupta <135385611+Priyanshraj26@users.noreply.github.com>
2025-09-07 18:24:48 +0530

    Icons

--- src/components/LinksSection.jsx
+++ src/components/LinksSection.jsx
@@ -1,12 +1,28 @@
+import { Instagram, Github, Linkedin, Twitter } from "lucide-react";
+
 export function LinksSection() {
   return (
     <div className="text-center text-white p-4">
       <h2 className="text-2xl font-bold">Profiles & Resume</h2>
       <ul className="mt-2 space-y-2">
-        <li><a href="https://github.com/yourprofile" target="_blank" className="underline">GitHub</a></li>
-        <li><a href="https://linkedin.com/in/yourprofile" target="_blank" className="underline">LinkedIn</a></li>
-        <li><a href="/projects/resume.pdf" target="_blank" className="underline">Resume</a></li>
+        <li>      <a href="https://www.instagram.com/" target="_blank">
+          <Instagram />
+        </a></li>
+        <li>            <a href="https://www.instagram.com/" target="_blank">
+          <Github />
+        </a></li>
+        <li>
+          <a href="https://www.instagram.com/" target="_blank">
+            <Linkedin />
+          </a></li>
+        <li>
+          <a href="https://www.instagram.com/" target="_blank">
+            <Twitter />
+          </a></li>
       </ul>
+
     </div>
   );
 }
+
+
