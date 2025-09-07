30f29913e75207a23e741ba2c06b8239f6bb177b
Priyansh Raj Gupta <135385611+Priyanshraj26@users.noreply.github.com>
2025-09-07 16:44:22 +0530

    UI Changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import MagicBento from "../components/MagicBento"; // Adjust path as needed
 import {Description} from "../components/Description";
 import {ProfilePicture} from "../components/ProfilePicture";
 import {LeetcodeSection} from "../components/LeetcodeSection";
@@ -7,48 +8,114 @@ import {LinksSection} from "../components/LinksSection";
 import { ContactSection } from "../components/ContactSection";
 import {ProjectsSection} from "../components/ProjectsSection";
 
+import { ParticleCard } from "../components/MagicBento";
+
 function Home() {
+  const glowColor = "132, 0, 255";
+  const particleCount = 12;
+  
   return (
     <div className="min-h-screen w-full p-6">
-      <div className="grid h-full w-full grid-cols-10 grid-rows-5 gap-4">
-        
-        {/* Project Section */}
-        <div className="col-span-4 row-span-3 rounded-3xl bg-orange-400 flex items-center justify-center">
-          <ProjectsSection/>
-        </div>
-        
-        {/* Description */}
-        <div className="col-span-5 row-span-1 rounded-3xl bg-orange-400 flex items-center justify-center">
-          <Description />
-        </div>
+      <MagicBento
+        textAutoHide={true}
+        enableStars={false} // We'll handle particles individually
+        enableSpotlight={true}
+        enableBorderGlow={true}
+        enableTilt={true}
+        enableMagnetism={true}
+        clickEffect={true}
+        spotlightRadius={300}
+        particleCount={particleCount}
+        glowColor={glowColor}
+      >
+        <div className="grid h-full w-full grid-cols-10 grid-rows-5 gap-4">
+          
+          {/* Project Section */}
+          <ParticleCard
+            className="col-span-4 row-span-3 rounded-3xl bg-[#060010] flex items-center justify-center magic-bento-card magic-bento-card--border-glow"
+            particleCount={particleCount}
+            glowColor={glowColor}
+            enableTilt={true}
+            clickEffect={true}
+            enableMagnetism={true}
+          >
+            <ProjectsSection/>
+          </ParticleCard>
+          
+          {/* Profile Picture */}
+          <ParticleCard
+            className="col-span-1 row-span-1 rounded-3xl bg-[#060010] flex items-center justify-center magic-bento-card magic-bento-card--border-glow"
+            particleCount={8}
+            glowColor={glowColor}
+            enableTilt={true}
+            clickEffect={true}
+            enableMagnetism={true}
+          >
+            <ProfilePicture />
+          </ParticleCard>
 
-        {/* Profile Picture */}
-        <div className="col-span-1 row-span-1 rounded-3xl bg-orange-400 flex items-center justify-center">
-          <ProfilePicture />
+          {/* Description */}
+          <ParticleCard
+            className="col-span-5 row-span-1 rounded-3xl bg-[#060010] flex items-center justify-center magic-bento-card magic-bento-card--border-glow"
+            particleCount={particleCount}
+            glowColor={glowColor}
+            enableTilt={true}
+            clickEffect={true}
+            enableMagnetism={true}
+          >
+            <Description />
+          </ParticleCard>
+
+          {/* Leetcode Section */}
+          <ParticleCard
+            className="col-span-2 row-span-2 rounded-3xl bg-[#060010] flex items-center justify-center magic-bento-card magic-bento-card--border-glow"
+            particleCount={10}
+            glowColor={glowColor}
+            enableTilt={true}
+            clickEffect={true}
+            enableMagnetism={true}
+          >
+            <LeetcodeSection />
+          </ParticleCard>
+
+          {/* Tech Stack */}
+          <ParticleCard
+            className="col-span-4 row-span-2 rounded-3xl bg-[#060010] flex items-center justify-center magic-bento-card magic-bento-card--border-glow"
+            particleCount={particleCount}
+            glowColor={glowColor}
+            enableTilt={true}
+            clickEffect={true}
+            enableMagnetism={true}
+          >
+            <SkillsSection />
+          </ParticleCard>
+
+          {/* Links + Resume */}
+          <ParticleCard
+            className="col-span-4 row-span-2 rounded-3xl bg-[#060010] flex items-center justify-center magic-bento-card magic-bento-card--border-glow"
+            particleCount={10}
+            glowColor={glowColor}
+            enableTilt={true}
+            clickEffect={true}
+            enableMagnetism={true}
+          >
+            <LinksSection />
+          </ParticleCard>
+
+          {/* Contact Section */}
+          <ParticleCard
+            className="col-span-6 row-span-2 rounded-3xl bg-[#060010] flex items-center justify-center magic-bento-card magic-bento-card--border-glow"
+            particleCount={particleCount}
+            glowColor={glowColor}
+            enableTilt={true}
+            clickEffect={true}
+            enableMagnetism={true}
+          >
+            <ContactSection />
+          </ParticleCard>
+
         </div>
-
-        {/* Leetcode Section */}
-        <div className="col-span-2 row-span-2 rounded-3xl bg-orange-400 flex items-center justify-center">
-          <LeetcodeSection />
-        </div>
-
-        {/* Tech Stack */}
-        <div className="col-span-4 row-span-2 rounded-3xl bg-orange-400 flex items-center justify-center">
-          <SkillsSection />
-        </div>
-
-        {/* Links + Resume */}
-        <div className="col-span-4 row-span-2 rounded-3xl bg-orange-400 flex items-center justify-center">
-          <LinksSection />
-        </div>
-
-        {/* Contact Section */}
-        <div className="col-span-6 row-span-2 rounded-3xl bg-orange-400 flex items-center justify-center">
-          <ContactSection />
-        </div>
-
-
-      </div>
+      </MagicBento>
     </div>
   );
 }
