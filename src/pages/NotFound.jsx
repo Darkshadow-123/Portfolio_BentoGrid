00c6bdaca16b07db029b8a155debaee0785f87b0
Rishi Lalwani <Rishi.123.lalwani@gmail.com>
2025-09-09 22:54:14 +0530

    Error Page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,14 @@
+import Noise from "../components/Noise";
 const NotFound = () => {
-    return <div>Not Found</div>;
-};
-
+    return ( 
+    <div className="relative w-[1720pxpx] h-[620px] overflow-hidden">
+    <Noise
+    patternSize={250}
+    patternScaleX={1}
+    patternScaleY={1}
+    patternRefreshInterval={2}
+    patternAlpha={15}
+    ></Noise>
+</div>
+)}
 export default NotFound;
